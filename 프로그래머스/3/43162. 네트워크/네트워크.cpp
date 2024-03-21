4570093d32097010b0ff00c30752bdf4c5d02e8e
yxnxx <100773990+yxnxx@users.noreply.github.com>
2024-03-22 01:32:55 +0900

    Update 네트워크.cpp
    add 틀렸던 이유

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -9,10 +9,14 @@ int con = 0;
 
 void DFS(int ind, vector<vector<int>> computers) {
     visited[ind] = true;
+    /*
+        i = ind+1부터 해서 틀렸었다!
+        예를 들어, 이런 경우를 고려하지 않은 것.
+        1 -> 5 -> 2
+    */
     for(int i = 0; i < computers.size(); i++) {
         if(computers[ind][i] == 1 && visited[i] == false) {
             DFS(i, computers);
-            cout << con << endl;
             con--;
         } 
     }
