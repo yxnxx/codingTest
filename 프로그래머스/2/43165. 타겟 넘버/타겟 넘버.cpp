d1dd2ed03a2c7e1f3f4bd103da69740792e1cfda
yxnxx <100773990+yxnxx@users.noreply.github.com>
2024-03-22 00:36:43 +0900

    Update 타겟 넘버.cpp
    오류 설명 추가

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -7,13 +7,15 @@ using namespace std;
 int cnt = 0;
 
 void DFS(int sum, int ind, vector<int> numbers, int target) {
+    // size()-1에서 종료하면 마지막 값은 계산에 포함이 안 돼서 실패
     if(ind == numbers.size()) {
         if(sum == target) {
             cnt++;
         }
         return;
     }
-    
+
+    // 증감연산자 사용시 ind 값이 누적돼서 오류!!!
     DFS(sum + numbers[ind], ind+1, numbers, target);
     DFS(sum - numbers[ind], ind+1, numbers, target);
 }
