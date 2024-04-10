db50be0eafca8160d438b46f661afa031fd3efa6
yxnxx <100773990+yxnxx@users.noreply.github.com>
2024-04-10 23:44:24 +0900

    [level 1] Title: 시저 암호, Time: 47.56 ms, Memory: 93.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        
+        
+        System.out.println((int)('Z'));
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') answer += " ";
+            else if(Character.isUpperCase(s.charAt(i))) {
+                if(s.charAt(i) + n > 'A' + 25) {
+                    answer += (char)('A' + (s.charAt(i) + n - 'A') % 25 - 1);
+                }
+                else answer += (char)(s.charAt(i) + n);
+            }
+            else {
+                if(s.charAt(i) + n > 'a' + 25) {
+                    answer += (char)('a' + (s.charAt(i) + n - 'a') % 25 - 1);
+                }
+                else answer += (char)(s.charAt(i) + n);
+            }
+            
+        }
+        return answer;
+    }
+}
