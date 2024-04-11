cc456ddf3613d6fb3c07055ed063ab0f80ad2efa
yxnxx <100773990+yxnxx@users.noreply.github.com>
2024-04-11 17:15:46 +0900

    Update 실패율.java

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -11,6 +11,7 @@ class Solution {
         int user = stages.length;
         HashMap<Integer, Double> fail = new HashMap<>();
         for(int i = 1; i <= N; i++) {
+            // 이 if문 없으면 0/0에서 Nan이 저장되어서 실패!
             if(user == 0) {
                 fail.put(i, 0.0);
                 continue;
@@ -20,13 +21,16 @@ class Solution {
             user = user - sucUser;
         }
         
-        
+        // compare 함수 재정의 잘 익혀 두기
         List<Integer> failKey = new ArrayList<>(fail.keySet());
         failKey.sort(new Comparator<Integer>() {
             public int compare(Integer o1, Integer o2) {
+                // Double 비교 시 메소드 사용해야 함
                 if(Double.compare(fail.get(o1), fail.get(o2)) == 0) {
+                    // 실패율이 같으면 작은 스테이지부터
                     return o1 - o2;
                 }
+                // 실패율이 높은 것부터
                 else return fail.get(o2).compareTo(fail.get(o1));
             }
         });
