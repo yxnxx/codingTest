1617f4ae85ebd606893ce559ee1e59e80ce772aa
yxnxx <100773990+yxnxx@users.noreply.github.com>
2024-04-12 13:45:51 +0900

    Update 토마토.java

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,3 +1,8 @@
+/*
+* 나는 일별로 처리하게 되는 토마토를 큐에 담고, 해당 토마토에 대한 처리가 끝나면 BFS 함수를 다시 호출하는 식으로 구현
+* 그러나 토마토 위치와 date 정보까지 담은 클래스를 만들거나, 이차원 배열 자체에 date 정보를 담아서 구현할 수 있었음!
+*/
+
 import java.util.*;
 import java.io.*;
 
@@ -30,26 +35,26 @@ class Main {
 
         tomato = new int[N][M];
         Queue<Pair> q = new LinkedList<>();
-        int boxes = N * M;
+        int tomatoNum = N * M;
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++) {
                 tomato[i][j] = Integer.parseInt(st.nextToken());
                 if(tomato[i][j] == 1) q.offer(new Pair(i, j));
-                if (tomato[i][j] == -1) boxes--;
+                if (tomato[i][j] == -1) tomatoNum--;
             }
         }
 
 
         date = 0;
         box = 0;
-        DFS(q);
+        BFS(q);
         
-        if(box != boxes) System.out.println(-1);
+        if(box != tomatoNum) System.out.println(-1);
         else System.out.println(date);
     }
 
-    static void DFS(Queue<Pair> q) {
+    static void BFS(Queue<Pair> q) {
         Queue<Pair> newQ = new LinkedList<>();
         while (!q.isEmpty()) {
             box++;
@@ -65,7 +70,7 @@ class Main {
             }
         }
         if(newQ.isEmpty()) return;
-        DFS(newQ);
+        BFS(newQ);
         date++;
     }
 }
