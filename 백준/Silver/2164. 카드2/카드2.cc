4068f06554d80d222cf3fbbbb42165f703aa9dec
yxnxx <100773990+yxnxx@users.noreply.github.com>
2024-04-28 19:45:49 +0900

    Update 카드2.cc

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -1,3 +1,10 @@
+/*
+* 처음에 문제 잘못 읽어서 틀렸음... 문제를 잘 읽자!
+* 나는 queue의 pop, push, front 함수를 활용하여 구현
+* 규칙을 찾아서 풀 수도 있었음! 2*input - (input보다 크거나 같은 2의 제곱수)
+* 그러나 규칙을 찾기가 쉽지 않은 문제...
+*/
+
 #include <iostream>
 #include <queue>
 
